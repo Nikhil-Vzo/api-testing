9c8b26d9b21e5ab9fff9a1bdff57f1fb848d67ff
Nikhil Yadav <nikhilyadav200530@gmail.com>
2025-12-15 15:38:56 +0530

    api testing

---
+++ index.js
@@ -0,0 +1,35 @@
+const url="https://official-joke-api.appspot.com/random_joke";
+
+(async () => {
+  const data = await fetch(url);
+  const res = await data.json();
+  console.log(res.setup + "-" + res.punchline );
+
+  const testing = async() => {
+    const d1 = await fetch(url);
+    const d2 = await d1.json();
+    console.log(d2.setup + "..." + d2.punchline);
+  }
+
+  testing();
+
+
+  const add = (a,b) => console.log(a+b);
+  add(5,5);
+
+
+  var space = document.getElementById("ko");
+  space.value = `${res.setup} - ${res.punchline}`;
+  space.style.color = "blue";
+  space.style.border = "2px solid blue";
+  space.style.padding = "10px";
+})();
+
+
+
+
+
+
+
+
+
